8081769cc9d25fa1f8e050f5bc3ea7ce26708921
JayXie9499 <50140372+JayXie9499@users.noreply.github.com>
2026-04-05 15:55:23 +0800

    fix(products): properly apply cdnUrl for product images

--- src/lib/data/products.ts
+++ src/lib/data/products.ts
@@ -1,35 +1,35 @@
 import * as m from '$lib/paraglide/messages';
-import { CDN_ASSETS } from '$lib/utils/cdn';
+import { CDN_ASSETS, cdnUrl } from '$lib/utils/cdn';
 
 export const productCategories = [
 	{
 		id: 'beakers',
 		name: () => m.category_beakers_flasks(),
 		gridArea: 'beakers',
-		image: CDN_ASSETS.productBeakers
+		image: cdnUrl(CDN_ASSETS.productBeakers)
 	},
 	{
 		id: 'tubes',
 		name: () => m.category_test_tubes(),
 		gridArea: 'tubes',
-		image: CDN_ASSETS.productTubes
+		image: cdnUrl(CDN_ASSETS.productTubes)
 	},
 	{
 		id: 'funnels',
 		name: () => m.category_funnels(),
 		gridArea: 'funnels',
-		image: CDN_ASSETS.productFunnels
+		image: cdnUrl(CDN_ASSETS.productFunnels)
 	},
 	{
 		id: 'condensers',
 		name: () => m.category_condensers(),
 		gridArea: 'condensers',
-		image: CDN_ASSETS.productCondensers
+		image: cdnUrl(CDN_ASSETS.productCondensers)
 	},
 	{
 		id: 'hydrometers',
 		name: () => m.category_hydrometers(),
 		gridArea: 'hydrometers',
-		image: CDN_ASSETS.productHydrometers
+		image: cdnUrl(CDN_ASSETS.productHydrometers)
 	}
 ] as const;
